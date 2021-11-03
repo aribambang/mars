76f9b2a389c96a2d89ea0377443caa453a4f5868
Ari Bambang Kurniawan <aribk210@gmail.com>
2021-11-03 19:08:22 +0700

    displaying movie detail

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -1,21 +1,56 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    setMovie({
-      id: props.match.params.id,
-      title: 'some title',
-      runtime: 140,
-    });
+    getDataMovies();
   }, [props]);
 
-  return (
+  const getDataMovies = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(`http://localhost:4000/v1/movies/${props.match.params.id}`);
+      response.data.movie.genres
+        ? (response.data.movie.genres = Object.values(response.data.movie.genres))
+        : (response.data.movie.genres = []);
+      setMovie(response.data.movie);
+
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+      setError('Invalid response code: ' + err.response?.status);
+      setIsLoading(false);
+    }
+  };
+
+  return isLoading ? (
+    <p>Loading ...</p>
+  ) : error ? (
+    <>Error: {error}</>
+  ) : (
     <>
       <h2>
-        Movie: {movie.title} {movie.id}
+        Movie: {movie.title} ({movie.year})
       </h2>
+      <div className='float-start'>
+        <small>Rating: {movie.mpaa_rating}</small>
+      </div>
+      <div className='float-end'>
+        {movie.genres &&
+          movie.genres.length > 0 &&
+          movie.genres.map((mg, index) => (
+            <span className='badge bg-secondary me-1' key={index}>
+              {mg}
+            </span>
+          ))}
+      </div>
+      <div className='clearfix'></div>
+      <hr />
+
       <table className='table table-compact table-striped'>
         <thead></thead>
         <tbody>
@@ -24,6 +59,12 @@ const Movie = (props) => {
               <strong>Title : </strong>
             </td>
             <td>{movie.title}</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>Description : </strong>
+            </td>
+            <td>{movie.description}</td>
           </tr>
           <tr>
             <td>
